05d60c0eb260b31ffe9473068313a936d7953db8
Ifeanyi Okadigbo <Ifeanyi Okadigbo>
2016-03-28 17:28:42 -0400

    adde back

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
 //app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //app.use(express.bodyParser());
 var mongojs = require('mongojs');
